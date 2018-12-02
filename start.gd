2b528f6321e40be5686ddadddb1577130cc07ed7
John Watson <john@watson-net.com>
2018-12-02 15:14:26 -0800

    Added credits, source link

--- start.gd
+++ start.gd
@@ -11,3 +11,11 @@ func _on_StartBtn_pressed():
 
 func _on_InstructionsButton_pressed():
 	get_tree().change_scene("res://instructions.tscn")
+
+
+func _on_yafdButton_pressed():
+	OS.shell_open("https://twitter.com/yafd")
+
+
+func _on_githubButton_pressed():
+	OS.shell_open("https://github.com/jotson/ld43")
