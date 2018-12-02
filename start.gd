4c91f354b770c5f8b2939db529aa04e416469ac0
John Watson <john@watson-net.com>
2018-12-02 15:45:02 -0800

    UI polish on start screen

--- start.gd
+++ start.gd
@@ -2,7 +2,13 @@ extends Control
 
 
 func _ready():
+	$sfxPop.play()
+
 	$Berry.ripen()
+
+	$Tween.interpolate_property($Label, 'modulate', Color(1,1,1,0), Color(1,1,1,1), 2.0, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$Tween.interpolate_property($Label, 'rect_scale', Vector2(5,5), Vector2(1,1), 1.0, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	$Tween.start()
 
 
 func _on_StartBtn_pressed():
@@ -19,3 +25,32 @@ func _on_yafdButton_pressed():
 
 func _on_githubButton_pressed():
 	OS.shell_open("https://github.com/jotson/ld43")
+
+
+func _on_Label_mouse_entered():
+	$Tween.interpolate_property($Label, 'rect_scale', Vector2(1.1,1.1), Vector2(1,1), 1.0, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	$Tween.start()
+
+
+func _on_Label_mouse_exited():
+	$Tween.interpolate_property($Label, 'rect_scale', Vector2(1.1,1.1), Vector2(1,1), 1.0, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	$Tween.start()
+
+
+func _on_frogControl_mouse_entered():
+	ribit()
+
+
+func _on_frogControl_mouse_exited():
+	ribit()
+
+
+func _on_frogControl_gui_input(ev):
+	if ev is InputEventMouseButton and ev.pressed:
+		ribit()
+
+
+func ribit():
+	$sfxRibit.play()
+	$Tween.interpolate_property($Frog/Sprite, 'scale', Vector2(2,1), Vector2(1,1), 1.0, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	$Tween.start()
